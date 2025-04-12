c1a020525bc84c1cda75d773f87ce0d8f66e2cc1
JohnMartin-19 <johnnkonge2020@gmail.com>
2025-04-12 06:06:25 +0300

    consumed the apis on the frontend, tweaking ui to be dynamic

--- Backend/main-app/mfarm/serializers.py
+++ Backend/main-app/mfarm/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from .models import *
 from accounts.models import CustomUser
 
+#pupose for the user serializer is to return a nested response.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email']
 class ProductSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Product
-        fields = ['name','price','description','quantity','image']
+        fields = ['name',"user",'price','description','quantity',"product_location",'image']
         read_only_fields = ['user']  #this prevents creating/setting user via API
 
     def create(self, validated_data):

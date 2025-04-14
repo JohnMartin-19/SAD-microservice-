9a9895383fd07140e9f53dceeb03b4201ba3a371
JohnMartin-19 <johnnkonge2020@gmail.com>
2025-04-15 02:13:55 +0300

    documented the api endpoints using swagger

--- Backend/main-app/m_farm/urls.py
+++ Backend/main-app/m_farm/urls.py
@@ -18,12 +18,23 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('mfarm/',include('mfarm.urls'))
+    path('mfarm/',include('mfarm.urls')),
+    # Schema
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # ReDoc (optional)
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 # Serve media files during development

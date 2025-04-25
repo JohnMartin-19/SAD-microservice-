0b43a6bb8c6b97dcaef3a1a26ff96dad4c95bfae
JohnMartin-19 <johnnkonge2020@gmail.com>
2025-04-25 08:16:41 +0300

    decoupled the main-app and initiall accounts app under it. Set up CI/CD Pipeline on AWS synching it with my repo

--- Backend/payment-service/payments/settings.py
+++ Backend/payment-service/payments/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local app installed
+    "pay.apps.PayConfig",
 ]
 
 MIDDLEWARE = [

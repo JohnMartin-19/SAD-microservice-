d96260db7b11353bba491a8a771417cdd8327982
John Mburu <johnnkonge2020@gmail.com>
2025-05-21 04:00:38 +0300

    Infra variables set

---
+++ Infra/terraform/variables.tf
@@ -0,0 +1,35 @@
+variable  "aws_region"{
+    default = "us-east-1"
+}
+
+variable "project_name"{
+    default  =  "M-FARM"
+}
+
+variable "vpc_cidr"{
+    default = "10.0.0.0/16"
+}
+
+variable "db_user_username"{
+    sensitive = true
+}
+
+variable "db_user_password"{
+    sensitive = true
+}
+
+variable "db_product_username"{
+    sensitive = true
+}
+
+variable "db_product_password"{
+    sensitive = true
+}
+
+variable "db_payment_username"{
+    sensitive = true
+}
+
+variable "db_payment_password"{
+    sensitive=true
+}

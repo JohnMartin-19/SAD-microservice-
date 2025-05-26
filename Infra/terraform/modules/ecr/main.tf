27d4811e8ec6d7f2fb461e3d1b913dd425252315
John Mburu <johnnkonge2020@gmail.com>
2025-05-26 09:52:52 +0300

    renamed ECR Repos

--- Infra/terraform/modules/ecr/main.tf
+++ Infra/terraform/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "repo" {
     for_each = toset(var.repositories)
-    name = "${var.project_name}/${each.value}"
+    name = "${var.project_name}-${each.value}"
     image_tag_mutability = "MUTABLE"
     image_scanning_configuration {
     scan_on_push = true

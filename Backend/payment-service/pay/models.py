1f0a57c913d4407274e2a285a4f6e8b3cb05dceb
John Mburu <johnnkonge2020@gmail.com>
2025-06-09 11:36:10 +0300

    made some migrations

--- Backend/payment-service/pay/models.py
+++ Backend/payment-service/pay/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Payment(models.Model):
-    user_id = models.IntegerField()  # Reference to user in UserService
+    user_id = models.IntegerField(null=True, blank=True)  
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('failed', 'Failed')])
     transaction_id = models.CharField(max_length=100)
